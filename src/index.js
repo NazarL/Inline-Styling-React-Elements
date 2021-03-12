0b027b4dddb83f58b700453d61fb83c88dfadc7d
NazarL <nazar.lebedenko@gmail.com>
2021-03-12 17:22:26 -0500

    add  custom style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+var greeting = "Good Morning!";
+const customStyle = {
+  color: "blue"
+};
+
+const currentTime = new Date().getHours();
+
+if (currentTime > 0 && currentTime < 12) {
+  customStyle.color = "red";
+} else if (currentTime > 12 && currentTime < 18) {
+  customStyle.color = "green";
+  greeting = "Good Afternoon!";
+} else {
+  greeting = "Good Evening!";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
